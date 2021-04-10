c079f8e5dfab14e63b948fba1d8b38772d94fa22
Nayane-19 <nayanemenezes2011@gmail.com>
2021-04-10 20:00:09 -0300

    :up: atualização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -6,7 +8,20 @@ export default function Home() {
     <Head>
       <title>Inicio | ig.news</title>
     </Head>
-    <h1>Hello World!</h1>
+
+    <main className= {styles.contentContainer}>
+      <section className= {styles.hero}>
+      <span> 👏 Olá, seja bem vindo(a)</span>
+      <h1>Notícias sobre o mundo do <span>React</span></h1>
+      <p>
+        Acesse todas as publicações  <br />
+        <span>por R$9,90 mensais</span>
+      </p>
+      <SubscribeButton />
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding"/>
+    </main>
     </>
   )
 }
