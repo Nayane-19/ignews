ccf4193b4094a74dc4944cedec365c259e53a619
Nayane-19 <nayanemenezes2011@gmail.com>
2021-04-11 12:16:20 -0300

    :package: nova funcionalidade

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,19 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import { FaProductHunt } from 'react-icons/fa'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
+ 
   return (
     <>
     <Head>
@@ -15,13 +26,31 @@ export default function Home() {
       <h1>Notícias sobre o mundo do <span>React</span></h1>
       <p>
         Acesse todas as publicações  <br />
-        <span>por R$9,90 mensais</span>
+        <span>por {product.amount} mensais</span>
       </p>
-      <SubscribeButton />
+      <SubscribeButton priceId = {product.priceId}/>
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding"/>
     </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1If12qKXFjtnBn1wiHYpacLM')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+  }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      product,
+  },
+  revalidate: 60 * 60 * 24, //24 horas
+}
 }
